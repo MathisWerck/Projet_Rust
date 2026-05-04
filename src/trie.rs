7956b991468433b7b5fe0c770dd9427de9f7f57f
Léo MARIAGE <leo.mariage@ekium.eu>
2026-05-04 22:24:59 +0200

    TrieNade

--- src/trie.rs
+++ src/trie.rs
@@ -2,17 +2,3 @@ pub struct TrieNode {
     pub children: Vec<(char, TrieNode)>,
     pub name: Option<String>,
 }
-
-pub struct Trie {
-    pub root: TrieNode,
-}
-
-impl Trie {
-    pub fn new() -> Trie {
-        todo!()
-    }
-
-    pub fn insert(&mut self, _number: &str, _name: &str) {
-        todo!()
-    }
-}
