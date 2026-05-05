1b70855f15cd9c8cb9d00c97d1c4370a379793fd
Léo MARIAGE <leo.mariage@ekium.eu>
2026-05-05 18:04:26 +0200

    Trie+new

--- src/trie.rs
+++ src/trie.rs
@@ -2,3 +2,40 @@ pub struct TrieNode {
     pub children: Vec<(char, TrieNode)>,
     pub name: Option<String>,
 }
+
+impl TrieNode {
+    pub fn new() -> TrieNode {
+        TrieNode {
+            children: Vec::new(),
+            name: None,
+        }
+    }
+}
+
+impl Default for TrieNode {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+pub struct Trie {
+    pub root: TrieNode,
+}
+
+impl Trie {
+    pub fn new() -> Trie {
+        Trie {
+            root: TrieNode::new(),
+        }
+    }
+
+    pub fn insert(&mut self, _number: &str, _name: &str) {
+        todo!()
+    }
+}
+
+impl Default for Trie {
+    fn default() -> Self {
+        Self::new()
+    }
+}
